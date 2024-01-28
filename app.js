64e4b5b0a1e74f91ca5764a4ae7acef556ccf80d
rirefat <rirefat.official@gmail.com>
2024-01-28 21:36:28 +0600

    adding a smooth effect while displaying the qr img

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const qrImg = document.getElementById("qrImg");
 const qrText = document.getElementById("qrText")
 
 function generateQR() {
-    // qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText.value}`;
-    qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText;
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText.value}`;
+    imgBox.classList.add("show-img")
    
 }
