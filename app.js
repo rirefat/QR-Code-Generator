156d491c39fc40ee7e01e195d6d86e7328a80e1c
rirefat <rirefat.official@gmail.com>
2024-01-28 21:59:13 +0600

    adding an animation for empty text box

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const qrImg = document.getElementById("qrImg");
 const qrText = document.getElementById("qrText")
 
 function generateQR() {
-    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText.value}`;
-    imgBox.classList.add("show-img")
-   
+    if (qrText.value.length > 0) {
+        qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText.value}`;
+        imgBox.classList.add("show-img")
+    }else{
+        qrText.classList.add("error")
+    }
 }
